d734be161ea9a0172535a75fb239611fe2c2ce37
Victor <victormeunier.dev@gmail.com>
2020-05-18 17:44:12 +0200

    [hang] change reset player

--- 2D/hang/game.gd
+++ 2D/hang/game.gd
@@ -4,11 +4,13 @@ onready var player = $Player
 onready var joints = $Joints
 onready var winscreen = $CanvasLayer/WinScreen
 onready var levels = $Levels
-onready var player_pos = $PlayerPos.global_position
 onready var camera = $Camera2D
 onready var player_camera = $Player/Camera2D
+onready var hud = $CanvasLayer/HUD
 
-var curr_level_nb = 6
+const player_scene = preload("res://player.tscn")
+
+var curr_level_nb = 0
 var curr_level
 
 # Called when the node enters the scene tree for the first time.
@@ -17,6 +19,7 @@ func _ready():
 	player.connect("anchor", self, "on_anchor")
 	player.connect("free", self, "on_free")
 	player.connect("die", self, "on_die")
+	player.connect("swipe", self, "on_swipe")
 
 func on_anchor(point, collider):
 	# First, remove the joint
@@ -35,28 +38,29 @@ func on_anchor(point, collider):
 	joints.add_child(joint)
 	
 func remove_anchor():
-	joints.remove_child(joints.get_child(0))
+	if joints.get_child_count() > 0:
+		joints.remove_child(joints.get_child(0))
 	
 func load_next_level():
 	curr_level_nb += 1
 	print("level_"+str(curr_level_nb))
-	var lvl = load("res://levels/level_" +str(curr_level_nb)+ ".tscn")
+	var lvl = "res://levels/level_" +str(curr_level_nb)+ ".tscn"
+	load_level(lvl)
+
+func load_previous_level():
+	curr_level_nb -= 1
+	print("level_"+str(curr_level_nb))
+	var lvl = "res://levels/level_" +str(curr_level_nb)+ ".tscn"
+	load_level(lvl)
+
+func load_level(level_str):
+	var lvl = load(level_str)
 	if lvl == null: return
 	# Remove curr level
 	if levels.get_child_count() > 0:
 		var to_remove = levels.get_child(0)
 		levels.call_deferred("remove_child", to_remove)
-	# Add next level
-	var level = lvl.instance()
-	level.connect("ready", self, "on_level_ready")
-	levels.call_deferred("add_child", level)
-
-func load_level(level_str):
-	var lvl = load(level_str)
-	# Remove curr level
-	if levels.get_child_count() > 0:
-		var to_remove = levels.get_child(0)
-		levels.call_deferred("remove_child", to_remove)
+		call_deferred("remove_child", player)
 	# Add next level
 	var level = lvl.instance()
 	level.connect("ready", self, "on_level_ready")
@@ -64,13 +68,18 @@ func load_level(level_str):
 	
 func reset_current_level():
 	curr_level.reset()
-	player.reset(player_pos)
-	player_camera.zoom(1, 1)
-	camera.current = true
+	#player.reset(curr_level.get_node('PlayerPosition'))
+	reset_player()
 
 func reset_player():
-	player.global_position = player_pos
-	remove_anchor()
+	# Add player instance to node2D, to place it in space
+	player_camera.current = false
+	camera.current = true
+	if player:
+		remove_child(player)
+	var p = player_scene.instance()
+	p.connect("ready", self, "on_player_node_ready")
+	add_child(p)
 	
 func on_free():
 	if joints.get_child_count() > 0:
@@ -78,11 +87,11 @@ func on_free():
 
 func _on_Goal_body_entered(body):
 	if player.dead: return
+	player.contact_monitor = false
 	print(body.name)
 	$WinSound.play()
-	player.contact_monitor = false
-	# Reset player first to avoid colliding with next level
-	player.reset(player_pos)
+	#player.contact_monitor = false
+
 #	player.visible = false
 #	winscreen.visible = true
 	load_next_level()
@@ -91,9 +100,22 @@ func _on_Goal_body_entered(body):
 		joints.remove_child(joints.get_child(0))
 	
 func on_level_ready():
+	hud.set_level(curr_level_nb)
 	curr_level = levels.get_child(0)
 	# Connect after resetting position to avoid re-triggering
 	curr_level.get_node("Goal").connect("body_entered", self, "_on_Goal_body_entered")
+
+	reset_player()
+	
+func on_player_node_ready():
+	player = $Player
+	player_camera = $Player/Camera2D
+	player.global_position = curr_level.get_node("PlayerPosition").global_position
+	player.connect("anchor", self, "on_anchor")
+	player.connect("free", self, "on_free")
+	player.connect("die", self, "on_die")
+	player.connect("swipe", self, "on_swipe")
+	
 
 func on_die():
 	# Switch wall color
@@ -116,6 +138,7 @@ func on_die():
 			child.change_wall_color(child.COLORS.WALL_DEAD)
 			child.change_background_color(child.COLORS.BACKGROUND_DEAD)
 	
+	remove_anchor()
 	# Switch to player's camera
 	camera.current = false
 	player_camera.current = true
@@ -123,5 +146,14 @@ func on_die():
 	player_camera.shake(0.5, 15, 10)
 	$DeathTimer.start()
 
+func on_swipe(direction):
+	if direction == "right":
+		load_previous_level()
+	elif direction == "left":
+		load_next_level()
+
 func _on_DeathTimer_timeout():
 	reset_current_level()
+
+func _on_TouchScreenButton_pressed():
+	remove_anchor()
