5490034e53cbb09d16a1e8f4c15ac42608573306
Victor <victormeunier.dev@gmail.com>
2020-05-19 00:49:40 +0200

    [hang] add default hook number

--- 2D/hang/gui/hud.gd
+++ 2D/hang/gui/hud.gd
@@ -4,10 +4,13 @@ onready var level = $LevelLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_hooks("∞")
+	set_hooks(-1)
 
 func set_level(level_nb):
 	level.text = "Level " + str(level_nb)
 
 func set_hooks(hooks_nb):
-	$HBoxContainer/HooksLabel.text = str(hooks_nb)
+	if hooks_nb != -1:
+		$HBoxContainer/HooksLabel.text = str(hooks_nb)
+	else:
+		$HBoxContainer/HooksLabel.text = str("∞")
