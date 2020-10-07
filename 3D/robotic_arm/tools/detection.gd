924accfe07771561cc4d5d92ddc009eb82c81184
mreliptik <victormeunier.dev@gmail.com>
2020-10-07 14:52:50 +0200

    [3D/robotic_arm] add blob and detection class

--- 3D/robotic_arm/tools/detection.gd
+++ 3D/robotic_arm/tools/detection.gd
@@ -1,16 +1,69 @@
-extends Node
+const Blob = preload("res://tools/blob.gd")
+const HSV = preload("res://tools/hsv.gd")
 
+func binarizeWithColor(args):
+	var im = args[0]
+	var color = args[1]
+	var tolerance = args[2]
+	
+	var binarized_im = Image.new()
+	binarized_im.copy_from(im)
+	
+	# Lock images before reading and setting
+	im.lock()
+	binarized_im.lock()
+	
+	for i in range(im.get_width()):
+		for j in range(im.get_height()):
+			var pixel = im.get_pixel(i,j)
+			var pixel_hsv = HSV.RGB_to_HSV(pixel)
+			var dist = pixel_hsv.distance_to(color)
+			#print(dist)
+			if dist < tolerance:
+				binarized_im.set_pixel(i, j, Color(1.0,1.0,1.0))
+			else:
+				binarized_im.set_pixel(i, j, Color(0.0,0.0,0.0))
+				
+	im.unlock()
+	binarized_im.unlock()
+	var binarized_texture = ImageTexture.new()
+	binarized_texture.create_from_image(binarized_im)
+	
+	return binarized_texture
+	
+func detectColorBlob(im, color):	
+	if !im: return
+	
+	# Lock image before reading
+	im.lock()
+	var binarized_im = Image.new()
+	binarized_im.copy_from(im)
+	im.unlock()
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+	var blobs = []
+	
+	var h = binarized_im.get_height()
+	var w = binarized_im.get_width()
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	binarized_im.lock()
+	
+	for i in range(h):
+		for j in range(w):
+			# Not white pixel, we don't care
+			var pixel = binarized_im.get_pixel(i, j)
+			#print(pixel)
+			if pixel == color:	
+				var found = false
+				# Loop through existing blobs
+				for blob in blobs:
+					if blob.is_near(i, j):
+						blob.add(i, j)
+						found = true
+						break
+				if !found:
+					var b = Blob.new()
+					b.blob(i, j)
+					blobs.append(b)
+	# Unlock when finished
+	binarized_im.unlock()
+	return blobs
--- 3D/robotic_arm/world.gd
+++ 3D/robotic_arm/world.gd
@@ -1,6 +1,7 @@
 extends Spatial
 
 const object = preload("res://object.tscn")
+const Detector = preload("res://tools/detection.gd")
 
 const MAX_FOV = 96
 const MIN_FOV = 33
@@ -10,7 +11,6 @@ export var spawn_time_interval = 1.0
 export var reference_color = Color("#fc1b04") 
 export var color_tolerance = 4.5
 
-const HSV = preload("res://tools/hsv.gd")
 onready var objects = $Room/Objects
 onready var reference_obj = $Room/Reference/ReferenceObject
 
@@ -24,6 +24,7 @@ var colors = [Color("#0C96BE"), Color("B023DD"), Color("857f10"), Color("#00FE3A
 var spawned_number = 0
 
 # CV & arm
+onready var detector = Detector.new()
 var blobs = null
 var target = null
 var target_grabbed = false
@@ -73,7 +74,7 @@ func _process(delta):
 	
 	# Get previous frame result
 	var compute_data = $ComputeViewport.get_texture().get_data()
-	blobs = detectColorBlob(compute_data, Color(1.0, 1.0, 1.0, 1.0))
+	blobs = detector.detectColorBlob(compute_data, Color(1.0, 1.0, 1.0, 1.0))
 	
 	if !blobs.empty():
 		var point = blobs[0].center()
@@ -131,107 +132,6 @@ func grab():
 			target_grabbed = true
 			$Room/robotic_arm/Armature/Skeleton/BoneAttachment2/Area.monitoring = false
 	
-func binarizeWithColor(args):
-	var im = args[0]
-	var color = args[1]
-	var tolerance = args[2]
-	
-	var binarized_im = Image.new()
-	binarized_im.copy_from(im)
-	
-	# Lock images before reading and setting
-	im.lock()
-	binarized_im.lock()
-	
-	for i in range(im.get_width()):
-		for j in range(im.get_height()):
-			var pixel = im.get_pixel(i,j)
-			var pixel_hsv = HSV.RGB_to_HSV(pixel)
-			var dist = pixel_hsv.distance_to(color)
-			#print(dist)
-			if dist < tolerance:
-				binarized_im.set_pixel(i, j, Color(1.0,1.0,1.0))
-			else:
-				binarized_im.set_pixel(i, j, Color(0.0,0.0,0.0))
-				
-	im.unlock()
-	binarized_im.unlock()
-	var binarized_texture = ImageTexture.new()
-	binarized_texture.create_from_image(binarized_im)
-	
-	return binarized_texture
-	
-func detectColorBlob(im, color):	
-	if !im: return
-	
-	# Lock image before reading
-	im.lock()
-	var binarized_im = Image.new()
-	binarized_im.copy_from(im)
-	im.unlock()
-
-	var blobs = []
-	
-	var h = binarized_im.get_height()
-	var w = binarized_im.get_width()
-
-	binarized_im.lock()
-	
-	for i in range(h):
-		for j in range(w):
-			# Not white pixel, we don't care
-			var pixel = binarized_im.get_pixel(i, j)
-			#print(pixel)
-			if pixel == color:	
-				var found = false
-				# Loop through existing blobs
-				for blob in blobs:
-					if blob.is_near(i, j):
-						blob.add(i, j)
-						found = true
-						break
-				if !found:
-					var b = Blob.new()
-					b.blob(i, j)
-					blobs.append(b)
-#
-	# Unlock when finished
-	binarized_im.unlock()
-#
-	return blobs
-
-class Blob:
-	var min_x : float
-	var min_y : float
-	var max_x : float
-	var max_y : float
-	
-	func blob(x, y):
-		min_x = x
-		min_y = y
-		max_x = x
-		max_y = y
-		
-	func rect():
-		return Rect2(min_x, min_y, max_x-min_x, max_y-min_y)
-		
-	func center():
-		return Vector2(rect().position.x + (rect().size.x/2), rect().position.y + (rect().size.y/2))
-		
-	func add(x, y):
-		min_x = min(min_x, x)
-		min_y = min(min_y, y)
-		max_x = max(min_x, x)
-		max_y = max(min_y, y)
-		
-	func is_near(x, y):
-		var cx = (min_x+max_x) / 2
-		var cy = (min_y+max_y) / 2
-		
-		var d = Vector2(cx, cy).distance_to(Vector2(x, y))
-		if d < 25: return true
-		else: return false
-
 func _on_SpawnIntervalTimer_timeout():
 	if spawned_number == object_number:
 		$SpawnIntervalTimer.stop()
@@ -242,7 +142,6 @@ func _on_SpawnIntervalTimer_timeout():
 		# Select a random location
 		instance.global_transform = $Room/Spawner.get_child(int(rand_range(0, $Room/Spawner.get_child_count()))).global_transform
 		
-		# TODO: Choose a color
 		instance.set_color(colors[int(rand_range(0, colors.size()))])
 		
 		objects.add_child(instance)
