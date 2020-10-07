d9e10c340bdc8415d76c1d6edbcf0047b3f1e748
mreliptik <victormeunier.dev@gmail.com>
2020-10-07 14:34:36 +0200

    [3D/robotic_arm] move HSV to class

--- 3D/robotic_arm/world.gd
+++ 3D/robotic_arm/world.gd
@@ -5,25 +5,25 @@ const object = preload("res://object.tscn")
 const MAX_FOV = 96
 const MIN_FOV = 33
 
-var mouse_sens = 0.15
-var zoom_sens = 3
-
 export var object_number = 10000
 export var spawn_time_interval = 1.0
 export var reference_color = Color("#fc1b04") 
 export var color_tolerance = 4.5
-export var thread_number = 12
 
+const HSV = preload("res://tools/hsv.gd")
 onready var objects = $Room/Objects
 onready var reference_obj = $Room/Reference/ReferenceObject
 
+# Camera zoom & rotation
+var mouse_sens = 0.15
+var zoom_sens = 3
 var middle_button_clicked = false
 
+# Object colors
 var colors = [Color("#0C96BE"), Color("B023DD"), Color("857f10"), Color("#00FE3A")]
-
 var spawned_number = 0
-onready var reference_color_hsv = RGBtoHSV(reference_color)
 
+# CV & arm
 var blobs = null
 var target = null
 var target_grabbed = false
@@ -31,7 +31,7 @@ var target_grabbed = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	
+		
 	$Room/robotic_arm/Armature/Skeleton/SkeletonIK.start(true)
 	
 	reference_obj.get_surface_material(0).albedo_color = reference_color
@@ -146,7 +146,7 @@ func binarizeWithColor(args):
 	for i in range(im.get_width()):
 		for j in range(im.get_height()):
 			var pixel = im.get_pixel(i,j)
-			var pixel_hsv : HSV = RGBtoHSV(pixel)
+			var pixel_hsv = HSV.new().from_RGB(pixel)
 			var dist = pixel_hsv.distance_to(color)
 			#print(dist)
 			if dist < tolerance:
@@ -233,43 +233,43 @@ class Blob:
 		else: return false
 	
 			
-func RGBtoHSV(color_rgb: Color) -> HSV:
-	var hsv = HSV.new()
-	var _min
-	var _max
-	var _delta
-	
-	_min = color_rgb.r if color_rgb.r < color_rgb.g else color_rgb.g 
-	_min = _min if _min < color_rgb.b else color_rgb.b
-	
-	_max = color_rgb.r if color_rgb.r > color_rgb.g else color_rgb.g 
-	_max = _max if _max > color_rgb.b else color_rgb.b
-	
-	hsv.v = _max
-	_delta = _max - _min
-	if (_delta < 0.00001):
-		hsv.s = 0
-		hsv.h = 0
-		return hsv
-	if _max > 0.0:
-		hsv.s = _delta / _max
-	else:
-		hsv.s = 0.0
-		hsv.h = NAN
-		return hsv
-	if color_rgb.r >= _max:
-		hsv.h = (color_rgb.g - color_rgb.b) / _delta
-	elif color_rgb.g >= _max:
-		hsv.h = 2.0 + (color_rgb.b - color_rgb.r) / _delta
-	else:
-		hsv.h = 4.0 + (color_rgb.r - color_rgb.g) / _delta
-		
-	hsv.h *= 60.0
-	
-	if hsv.h < 0.0:
-		hsv.h += 360.0
-	
-	return hsv
+#func RGBtoHSV(color_rgb: Color) -> HSV:
+#	var hsv = HSV.new()
+#	var _min
+#	var _max
+#	var _delta
+#
+#	_min = color_rgb.r if color_rgb.r < color_rgb.g else color_rgb.g 
+#	_min = _min if _min < color_rgb.b else color_rgb.b
+#
+#	_max = color_rgb.r if color_rgb.r > color_rgb.g else color_rgb.g 
+#	_max = _max if _max > color_rgb.b else color_rgb.b
+#
+#	hsv.v = _max
+#	_delta = _max - _min
+#	if (_delta < 0.00001):
+#		hsv.s = 0
+#		hsv.h = 0
+#		return hsv
+#	if _max > 0.0:
+#		hsv.s = _delta / _max
+#	else:
+#		hsv.s = 0.0
+#		hsv.h = NAN
+#		return hsv
+#	if color_rgb.r >= _max:
+#		hsv.h = (color_rgb.g - color_rgb.b) / _delta
+#	elif color_rgb.g >= _max:
+#		hsv.h = 2.0 + (color_rgb.b - color_rgb.r) / _delta
+#	else:
+#		hsv.h = 4.0 + (color_rgb.r - color_rgb.g) / _delta
+#
+#	hsv.h *= 60.0
+#
+#	if hsv.h < 0.0:
+#		hsv.h += 360.0
+#
+#	return hsv
 
 func _on_SpawnIntervalTimer_timeout():
 	if spawned_number == object_number:
@@ -289,25 +289,7 @@ func _on_SpawnIntervalTimer_timeout():
 
 func _on_ColorPickerButton_color_changed(color):
 	reference_color = color
-	reference_color_hsv = RGBtoHSV(color)
 	reference_obj.get_surface_material(0).albedo_color = color
-
-func _on_ThreadPool_task_finished(task_tag):
-	pass
-	
-# Thread must be disposed (or "joined"), for portability.
-func _exit_tree():
-	pass
-	#thread.wait_to_finish()
-	
-class HSV:
-	var h: float
-	var s: float
-	var v: float
-	
-	func distance_to(hsv_color: HSV):
-		return Vector3(h, s, v).distance_to(Vector3(hsv_color.h, hsv_color.s, hsv_color.v))
-
 
 func _on_FutureThreadPool_task_completed(task):
 	var _task = task
