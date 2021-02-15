346bc8c46a93b6d34a3d3aa5ff405248cf42163c
Victor <victormeunier.dev@gmail.com>
2021-02-15 10:31:06 +0100

    [3D/hoverboard] add hoverboard model

--- 3D/hoverboard/scenes/hoverboard.gd
+++ 3D/hoverboard/scenes/hoverboard.gd
@@ -2,8 +2,8 @@ extends RigidBody
 
 onready var points = get_tree().get_nodes_in_group("Points")
 
-var speed = 50
-var reverse_speed = 30
+var speed = 800
+var reverse_speed = 700
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,7 +21,8 @@ func _physics_process(delta):
 			var collision_point = ray.get_collision_point()
 			
 			# Calculate distance between point and raycast hit
-			var dist = collision_point.distance_to(point.global_transform.origin)
+			#var dist = collision_point.distance_to(point.global_transform.origin)
+			var dist = collision_point.distance_squared_to(point.global_transform.origin)
 			var force_vec = (point.global_transform.origin - collision_point).normalized()
 			
 			# Apply force based on distance
@@ -30,19 +31,19 @@ func _physics_process(delta):
 			
 			var board_pos = get_global_transform().origin
 			var thruster_pos = $ThrusterPos.get_global_transform().origin
-			# Apply force to move
-			if Input.is_action_pressed("forward"):
-				#add_force(-global_transform.basis.x * speed * delta, board_pos - thruster_pos)
-				add_central_force(-global_transform.basis.x * speed * delta)
-				#add_central_force(-transform.basis.x * speed * delta)
-				#apply_central_impulse(-global_transform.basis.x * speed * delta)
-			if Input.is_action_pressed("back"):
-				#add_force(global_transform.basis.x * reverse_speed * delta, board_pos - thruster_pos)
-				add_central_force(transform.basis.x * speed * delta)
-			
-			# Apply torque to rotate
-			if Input.is_action_pressed("forward"):
-				pass
-				#add_torque()
-			if Input.is_action_pressed("back"):
-				pass
+	# Apply force to move
+	if Input.is_action_pressed("forward"):
+		#add_force(-global_transform.basis.x * speed * delta, board_pos - thruster_pos)
+		add_central_force(-global_transform.basis.x * speed * delta)
+		#add_central_force(-transform.basis.x * speed * delta)
+		#apply_central_impulse(-global_transform.basis.x * speed * delta)
+	if Input.is_action_pressed("back"):
+		#add_force(global_transform.basis.x * reverse_speed * delta, board_pos - thruster_pos)
+		add_central_force(transform.basis.x * speed * delta)
+	
+	# Apply torque to rotate
+	if Input.is_action_pressed("right"):
+		add_torque(-global_transform.basis.y * 800 * delta)
+		#add_torque()
+	if Input.is_action_pressed("left"):
+		add_torque(global_transform.basis.y * 800 * delta)
