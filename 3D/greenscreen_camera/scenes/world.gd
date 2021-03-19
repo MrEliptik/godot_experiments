9a3fd7121341fe224c19e80d6b82aaa116a1648e
Victor <victormeunier.dev@gmail.com>
2021-03-19 22:46:22 +0100

    [3D/greenscreen_camera] add background to greenscreen camera

--- 3D/greenscreen_camera/scenes/world.gd
+++ 3D/greenscreen_camera/scenes/world.gd
@@ -7,3 +7,6 @@ func _ready():
 func _process(delta):
 	$Viewport/Camera.look_at($Player.get_node("CameraTarget").global_transform.origin, Vector3.UP)
 	$Viewport2/Camera.look_at($Player.get_node("CameraTarget").global_transform.origin, Vector3.UP)
+	
+	$Screen.get_surface_material(0).next_pass.uv1_offset.x -= delta * 0.05
+	$Screen.get_surface_material(0).next_pass.uv1_offset.y += delta * 0.05
