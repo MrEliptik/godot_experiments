8670fe1a39ef50cf90e2b6a0b99b9ce3348ca325
Victor <victormeunier.dev@gmail.com>
2021-04-12 17:22:06 +0200

    [3D/hoverboard] move camera to follow board

--- 3D/hoverboard/scenes/hoverboard.gd
+++ 3D/hoverboard/scenes/hoverboard.gd
@@ -2,48 +2,57 @@ extends RigidBody
 
 onready var points = get_tree().get_nodes_in_group("Points")
 
-var speed = 800
-var reverse_speed = 700
+var speed = 15000
+var turn_speed = 2000
+var reverse_speed = 10000
+var hover_force = 500
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == BUTTON_WHEEL_UP:
+				hover_force += 20
+			if event.button_index == BUTTON_WHEEL_DOWN:
+				hover_force -= 20
+			hover_force = clamp(hover_force, 0, 1200)
 
 func _physics_process(delta):
 	# Loop through rays and points
 	for i in range(points.size()):
 		var point = points[i]
 		var ray = point.get_child(0)
-		ray.rotation = -rotation
 		ray.force_raycast_update()
 		if ray.is_colliding():
 			var collision_point = ray.get_collision_point()
 			
 			# Calculate distance between point and raycast hit
-			#var dist = collision_point.distance_to(point.global_transform.origin)
-			var dist = collision_point.distance_squared_to(point.global_transform.origin)
-			var force_vec = (point.global_transform.origin - collision_point).normalized()
+			var dist = collision_point.distance_to(point.global_transform.origin)
+			#var force_vec = (point.global_transform.origin - collision_point).normalized()
 			
 			# Apply force based on distance
-			add_force(Vector3.UP * (1/dist) * 10, point.global_transform.origin * delta)
-			#add_force(force_vec * (1/dist) * 10, point.global_transform.origin * delta)
+			add_force(Vector3.UP * (1/dist) * hover_force * delta, point.global_transform.origin - global_transform.origin)
 			
 			var board_pos = get_global_transform().origin
 			var thruster_pos = $ThrusterPos.get_global_transform().origin
+			
 	# Apply force to move
 	if Input.is_action_pressed("forward"):
-		#add_force(-global_transform.basis.x * speed * delta, board_pos - thruster_pos)
 		add_central_force(-global_transform.basis.x * speed * delta)
-		#add_central_force(-transform.basis.x * speed * delta)
-		#apply_central_impulse(-global_transform.basis.x * speed * delta)
 	if Input.is_action_pressed("back"):
-		#add_force(global_transform.basis.x * reverse_speed * delta, board_pos - thruster_pos)
-		add_central_force(transform.basis.x * speed * delta)
+		add_central_force(global_transform.basis.x * speed * delta)
 	
 	# Apply torque to rotate
 	if Input.is_action_pressed("right"):
-		add_torque(-global_transform.basis.y * 800 * delta)
-		#add_torque()
+		add_torque(-global_transform.basis.y * turn_speed * delta)
 	if Input.is_action_pressed("left"):
-		add_torque(global_transform.basis.y * 800 * delta)
+		add_torque(global_transform.basis.y * turn_speed * delta)
