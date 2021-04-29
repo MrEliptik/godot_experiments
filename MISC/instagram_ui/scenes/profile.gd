d7f921c1d70ba1c5cf2ecc18b010f65b1c0c518d
Victor <victormeunier.dev@gmail.com>
2021-04-30 00:36:51 +0200

    [MISC/instagram_ui] cleanup

--- MISC/instagram_ui/scenes/profile.gd
+++ MISC/instagram_ui/scenes/profile.gd
@@ -15,24 +15,17 @@ func _input(event):
 			scrolling_up = true
 
 	elif event is InputEventMouseButton:
-		#print(event.button_index)
 		if event.button_index == BUTTON_WHEEL_UP:
 			scrolling_up = true
-		#BUTTON_WHEEL_UP  
 
 func _process(delta):
-	#print(first_scroll.scroll_vertical)
-	#print(second_scroll.scroll_vertical)
-
 	if first_scroll.scroll_vertical > 1080 && first_content.visible:
 		first_scroll.scroll_vertical = 1080
 		first_content.visible = false
 		second_scroll.scroll_vertical_enabled = true
 	
-	elif scrolling_up && second_scroll.scroll_vertical == 0 && !first_content.visible:
-		first_content.visible = true
-		second_scroll.scroll_vertical_enabled = false
-	
+#	elif scrolling_up && second_scroll.scroll_vertical == 0 && !first_content.visible:
+#		first_content.visible = true
+#		second_scroll.scroll_vertical_enabled = false
 	
 	scrolling_up = false
-		#$VBoxContainer/ScrollContainer/VBoxContainer2/FirstScrollContent/HighlightsContainer/HBoxContainer2/VBoxContainer/StoryBtn.grab_focus()
